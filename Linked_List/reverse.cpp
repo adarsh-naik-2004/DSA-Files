dd83e5797363dd834f0d2f64312068e481158223
Adarsh Manjunath Naik <130145440+adarsh-naik-2004@users.noreply.github.com>
2024-09-17 23:47:32 +0530

    Update reverse.cpp

--- Linked_List/reverse.cpp
+++ Linked_List/reverse.cpp
@@ -6,9 +6,26 @@ Node* reverse(Node* prev,Node* curr){
     if(curr==NULL){
         return prev;
     }
-    Node* temp=crr->next;
+    Node* temp=curr->next;
     curr->next=prev;
     prev=curr;
     curr=temp;
     return reverse(prev,curr);
 }
+
+/*
+ iteratively ---> simple loop
+*/
+ListNode* reverseok(ListNode* head){
+    ListNode* curr=head;
+    ListNode* prev=NULL;
+
+    while(curr!=NULL){
+    ListNode* temp=curr->next;
+    curr->next=prev;
+    prev=curr;
+    curr=temp;
+    }  
+    return prev;
+}
+
