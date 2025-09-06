060a9e3489b33d48d948cc7eb24c30c735782d0e
Adarsh Naik <130145440+adarsh-naik-2004@users.noreply.github.com>
2025-09-06 17:25:29 +0530

    Update validate_bst.cpp

--- BST/validate_bst.cpp
+++ BST/validate_bst.cpp
@@ -1,3 +1,5 @@
+// 1st Approach --> Inorder sorted hai ki nahi check karlo
+
 class Solution {
 public:
 vector<int> ans;
@@ -17,17 +19,39 @@ vector<int> ans;
         inordertraversal(root);
         bool result = true;
         for (int i = 1; i < ans.size(); i++){
-        if (ans[i] <= ans[i-1])
-        {
-            result = false;
-            break;
+            if (ans[i] <= ans[i-1]){
+                result = false;
+                break;
+            }
         }
-    }
         if(result){
             return true;
         }
         else{
             return false;
         }
+    }
+};
+
+// 2nd approach --> ek range ke ander hona chahiye 
+
+class Solution {
+public:
+
+    bool solve(TreeNode* root,long long min,long long max){
+        if(root==NULL){
+            return true;
+        }
+        if(root->val > min && root->val < max){
+            bool left=solve(root->left,min,root->val);
+            bool right=solve(root->right,root->val,max);
+
+            return left && right;
+        }
+        return false;
+    }
+
+    bool isValidBST(TreeNode* root) {
+        return solve(root,LLONG_MIN,LLONG_MAX);
     }
 };
